2a29901d105c42997b12154ec750d2242e032032
Samruddhi Dharankar <samruddhee.dharankar@gmail.com>
2020-01-16 15:09:31 +0530

    successful php

--- src/login/login.php
+++ src/login/login.php
@@ -2,24 +2,24 @@
     $firstName = filter_input(INPUT_POST, 'firstName');
     $lastName = filter_input(INPUT_POST, 'lastName');
     $email = filter_input(INPUT_POST, 'email');
-    $password = filter_input(INPUT_POST, 'password');
+    $password1 = filter_input(INPUT_POST, 'password1');
 
     if (!empty($firstName)) {
         if(!empty($email)) {
-            if(!empty($password)) {
+            if(!empty($password1)) {
                 $host = "localhost";
                 $username = "root";
                 $dbpassword = "";
                 $dbname = "login";
 
                 //DB connection
-                $conn=new mysqli($host,$username,$dbpassword,$dbpassword);
+                $conn=new mysqli($host,$username,$dbpassword,$dbname);
 
                 if(mysqli_connect_error()) {
                     die('Connect Error ('.mysqli_connect_errno().')'.mysqli_connect_error());
                 }
                 else {
-                    $sql="INSERT INTO customer(firstName,lastName,email.password) values ('$firstName','$lastName','$email','$password')";
+                    $sql="INSERT INTO customer(firstName,lastName,email,password1) values ('$firstName','$lastName','$email','$password1')";
                     if($conn->query($sql)) {
                         echo "record inserted successfully";
                     }
